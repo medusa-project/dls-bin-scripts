397c0b2ed2ecf83f4d229e45f3ced4f4106736ca
Alex Dolski <alexd@n-ary.net>
2019-03-02 17:03:46 -0600

    Revert back to Passenger

--- env.sh
+++ env.sh
@@ -2,12 +2,3 @@
 
 export RAILS_ENV=production
 export DLS_HOME=$HOME/kumquat
-
-# apache will serve these
-export RAILS_SERVE_STATIC_FILES=false
-
-# number of puma workers
-export WEB_CONCURRENCY=8
-
-# number of threads per puma worker
-export RAILS_MAX_THREADS=16
